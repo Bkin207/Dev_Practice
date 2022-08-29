d48fa15b41dad1f38a55355feb3dae500cfb69f1
BrendenKing17 <bkin207@wgu.edu>
2022-08-29 07:33:21 -0600

    first version

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hi! please write something!");
+        int message = Integer.valueOf(scanner.nextLine());
+        int i = 5;
+        while (i>0) {
+            System.out.println(3/2 *1);
+            i--;
+        }
+    }
+}
